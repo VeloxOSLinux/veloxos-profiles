08cd78c493b9c58ca990c57a4da3a84efe6545d3
HrSkully <t.rosen@mailbox.org>
2026-02-04 17:14:48 +0100

    Edit the grub.cfg and calamares stylesheet

--- update.sh
+++ update.sh
@@ -3,5 +3,6 @@
 cp -r /home/user/velox-profiles/custom/gnome/* ./gnome/
 cp -r /home/user/velox-profiles/custom/shared/* ./shared/
 cp ~/.config/manjaro-tools/manjaro-tools.conf ./.config/
+cp -r /home/user/velox-profiles/manjaro/gnome ./base/
 
 echo "Files synced! Ready to git add, commit and push."
