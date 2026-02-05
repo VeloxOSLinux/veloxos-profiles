faf2d355da12ea8b4552cd4fcaaf7156786a8f8e
HrSkully <t.rosen@mailbox.org>
2026-02-05 09:50:26 +0100

    Add cosmic desktop

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ cp -r /home/user/velox-profiles/custom/gnome/* ./gnome/
 cp -r /home/user/velox-profiles/custom/shared/* ./shared/
 cp ~/.config/manjaro-tools/manjaro-tools.conf ./.config/
 cp -r /home/user/velox-profiles/manjaro/gnome ./base/
+cp -r /home/user/velox-profiles/custim/cosmic/* ./cosmic/
 
 echo "Files synced! Ready to git add, commit and push."
