4872102569234fe9ab879a079af947557235885d
skully <admin@veloxos.org>
2026-03-09 14:22:59 +0100

    add custom packages and remove unused

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Kopiert alles von den Arbeitsverzeichnissen ins Repo
-cp -r /home/user/velox-profiles/custom/shared/* ./shared/
+cp -r /home/user/iso-profiles/custom/shared/* ./shared/
 cp ~/.config/manjaro-tools/manjaro-tools.conf ./.config/
-cp -r /home/user/velox-profiles/manjaro/gnome ./base/
-cp -r /home/user/velox-profiles/custom/cosmic/* ./cosmic/
-cp -r /home/user/velox-profiles/custom/kde/* ./kde/
+#cp -r /home/user/iso-profiles/manjaro/gnome ./base/
+#cp -r /home/user/iso-profiles/custom/cosmic/* ./cosmic/
+cp -r /home/user/iso-profiles/custom/kde/* ./kde/
 
 echo "Files synced! Ready to git add, commit and push."
